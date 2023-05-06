021e800d8d0b0c4302a032858b6cafab144ce910
Yuta Osawa <yutaosawa81@gmail.com>
2023-05-06 23:03:53 +0900

    write some functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
+
+export const hello = functions.https.onCall((data, context) => {
+  return {
+    message: `Hello ${data.name ?? 'world'}!`,
+  }
+})
